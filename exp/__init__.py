c93d4c545ee0fe4929d0b8ba37268d0da161bb9d
Master-PLC <18358716036@163.com>
2026-07-01 21:03:12 +0800

    remove unrelated contents

--- exp/__init__.py
+++ exp/__init__.py
@@ -5,15 +5,9 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 from exp.exp_long_term_forecasting import Exp_Long_Term_Forecast
 from exp.exp_short_term_forecasting import Exp_Short_Term_Forecast
-from exp.exp_long_term_forecasting_trans import Exp_Long_Term_Forecast_Trans
-from exp.exp_long_term_forecasting_invtrans import Exp_Long_Term_Forecast_InvTrans
-from exp.exp_short_term_forecasting_trans import Exp_Short_Term_Forecast_Trans
 
 
 EXP_DICT = {
     'long_term_forecast': Exp_Long_Term_Forecast,
     'short_term_forecast': Exp_Short_Term_Forecast,
-    'long_term_forecast_trans': Exp_Long_Term_Forecast_Trans,
-    'long_term_forecast_invtrans': Exp_Long_Term_Forecast_InvTrans,
-    'short_term_forecast_trans': Exp_Short_Term_Forecast_Trans,
 }
